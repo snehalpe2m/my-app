bba1546a4681bc9606988f64c76ce4158f284e83
Snehal Pancholi <snehalp@e2msol.com>
2022-02-09 22:52:34 +0530

    out new
    out new

---
+++ out/_next/static/WIKQ7ysbcJdt3cMQjLVDF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcustom-page","\u002F","\u002Fposts","\u002Fcategory\u002F[categorySlug]","\u002Fposts\u002F[postSlug]\u002F[postCursor]","\u002Fposts\u002F[postSlug]","\u002Fcategory\u002F[categorySlug]\u002F[paginationTerm]\u002F[categoryCursor]","\u002F[...pageUri]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
